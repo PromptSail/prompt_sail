6c71d9961e9c566b7d1688ed6c60d6c442bf9c51
Przemysław Górecki <przemyslaw.gorecki@gmail.com>
2023-11-02 13:16:55 +0100

    enable code reload

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 import uvicorn
 
-from app.app import app
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
