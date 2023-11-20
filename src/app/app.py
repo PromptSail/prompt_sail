cebc214c844af57e95d305e414f8e3a15a44afaa
Szymon Śliwa <szymon242820@gmail.com>
2023-11-20 15:48:33 +0100

    hotfix precreated projects

--- src/app/app.py
+++ src/app/app.py
@@ -14,44 +14,43 @@ container.config.override(config)
 
 @asynccontextmanager
 async def fastapi_lifespan(app: FastAPI):
-    from projects.models import Project, AIProvider
+    from projects.schemas import CreateProjectSchema, ProjectAIProviderSchema
+    from projects.use_cases import add_project
 
     application = container.application()
     with application.transaction_context() as ctx:
         project_repository = ctx["project_repository"]
         if project_repository.count() == 0:
-            project_repository.add(
-                Project(
+            data1 = CreateProjectSchema(
                     name="Project 1",
-                    slug="project-1",
+                    slug="project1",
                     description="Project 1 description",
                     ai_providers=[
-                        AIProvider(
+                        ProjectAIProviderSchema(
                             api_base="https://api.openai.com/v1",
-                            provider_name="provider1",
-                            model_name="model1",
+                            provider_name="OpenAI",
+                            model_name="gpt-3.5-turbo",
                         ),
                     ],
                     tags=["tag1", "tag2"],
                     org_id="organization",
                 )
-            )
-            project_repository.add(
-                Project(
+            data2 = CreateProjectSchema(
                     name="Project 2",
-                    slug="project-2",
+                    slug="project2",
                     description="Project 2 description",
                     ai_providers=[
-                        AIProvider(
+                        ProjectAIProviderSchema(
                             api_base="https://api.openai.com/v1",
-                            provider_name="provider1",
-                            model_name="model1",
+                            provider_name="OpenAI",
+                            model_name="gpt-3.5-turbo",
                         ),
                     ],
-                    tags=["tag1", "tag2"],
+                    tags=["tag1", "tag2", "tag3"],
                     org_id="organization",
                 )
-            )
+            ctx.call(add_project, data1)
+            ctx.call(add_project, data2)
     yield
     ...
 
