aeb003f1c499313e6e82ce72ae2340f7282da11a
Adrian Bonislawski <adrian_bon@wp.pl>
2024-01-23 16:06:12 +0100

    login page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,31 +6,44 @@ import 'react-toastify/dist/ReactToastify.css';
 import Transaction from './pages/Transaction';
 import Sidebar from './components/Sidebar/Sidebar';
 import AllTransactions from './pages/AllTransactions';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Signin from './pages/Signin';
+import { checkLogin } from './storage/login';
 
 const App = () => {
     const page = useRef(null);
-    return (
-        <>
-            <div className="h-screen">
-                <Sidebar pageRef={page}></Sidebar>
-                <div
-                    ref={page}
-                    style={{ marginLeft: '350px' }}
-                    className="overflow-y-auto mx-[100px]"
-                >
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/projects/:projectId" element={<Project />} />
-                        <Route path="/transactions" element={<AllTransactions />} />
-                        <Route path="/transactions/:transactionId" element={<Transaction />} />
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
+    const [isLogged, setLoginState] = useState(checkLogin());
+    if (isLogged) {
+        return (
+            <>
+                <div className="h-screen">
+                    <Sidebar pageRef={page}></Sidebar>
+                    <div
+                        ref={page}
+                        style={{ marginLeft: '350px' }}
+                        className="overflow-y-auto mx-[100px]"
+                    >
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/projects/:projectId" element={<Project />} />
+                            <Route path="/transactions" element={<AllTransactions />} />
+                            <Route path="/transactions/:transactionId" element={<Transaction />} />
+                            <Route path="*" element={<Navigate to="/" />} />
+                        </Routes>
+                    </div>
                 </div>
-            </div>
-            <ToastContainer />
-        </>
-    );
+                <ToastContainer />
+            </>
+        );
+    } else
+        return (
+            <>
+                <Routes>
+                    <Route path="/signin" element={<Signin setLoginState={setLoginState} />} />
+                    <Route path="*" element={<Navigate to="/signin" />} />
+                </Routes>
+            </>
+        );
 };
 
 export default App;
---
+++ ui/src/pages/Signin.tsx
@@ -0,0 +1,62 @@
+import { useFormik } from 'formik';
+import { SetStateAction } from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { checkLogin } from '../storage/login';
+import Logo from '../assets/imgs/prompt_sail_logo200.jpg';
+const Signin: React.FC<{ setLoginState: (arg: SetStateAction<boolean>) => void }> = ({
+    setLoginState
+}) => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: () => {
+            localStorage.setItem('login', 'true');
+            setLoginState(checkLogin());
+        }
+    });
+    return (
+        <>
+            <div className="signin">
+                <div className="formContainer">
+                    <div className="form">
+                        <h1>Welcome to Ermlab</h1>
+                        <h4>Sign in to continue</h4>
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="inputs">
+                                <FloatingLabel label="Email">
+                                    <Form.Control
+                                        type="email"
+                                        name="email"
+                                        placeholder="email@example.com"
+                                        value={formik.values['email']}
+                                        onChange={formik.handleChange}
+                                        autoComplete="login email"
+                                    />
+                                </FloatingLabel>
+                                <FloatingLabel label="Password">
+                                    <Form.Control
+                                        type="password"
+                                        name="password"
+                                        placeholder="$tronGP@s$word"
+                                        value={formik.values['password']}
+                                        onChange={formik.handleChange}
+                                        autoComplete="login current-password"
+                                    />
+                                </FloatingLabel>
+                            </div>
+                            <Button type="submit">Log In</Button>
+                        </form>
+                    </div>
+                    <div>
+                        <img src={Logo} />
+                        <h3>Prompt Sail</h3>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Signin;
