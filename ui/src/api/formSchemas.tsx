96e88bd560fa2ea3fa7efd286b0792e74ca55b81
Adrian Bonislawski <adrian_bon@wp.pl>
2024-01-30 16:12:33 +0100

    Change validation messages to English

--- ui/src/api/formSchemas.tsx
+++ ui/src/api/formSchemas.tsx
@@ -1,18 +1,15 @@
 import * as yup from 'yup';
 export const projectSchema = yup.object().shape({
-    name: yup.string().required('To pole jest wymagane'),
-    slug: yup.string().required('To pole jest wymagane'),
-    description: yup.string().max(280, 'Za długie').required('To pole jest wymagane'),
-    tags: yup.string().required('To pole jest wymagane'),
+    name: yup.string().required('This field is required'),
+    slug: yup.string().required('This field is required'),
+    description: yup.string().max(280, 'Too long').required('This field is required'),
+    tags: yup.string().required('This field is required'),
     ai_providers: yup.array().min(1, 'You need to add at least one AI Provider')
 });
 
 export const providerSchema = yup.object().shape({
-    api_base: yup
-        .string()
-        .url('Proszę wpisać poprawny adres url')
-        .required('To pole jest wymagane'),
+    api_base: yup.string().url('Enter a valid url').required('This field is required'),
     provider_name: yup.string().required('This field is required'),
-    deployment_name: yup.string().required('To pole jest wymagane'),
+    deployment_name: yup.string().required('This field is required'),
     description: yup.string()
 });
