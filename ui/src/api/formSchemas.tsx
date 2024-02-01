fbb8198bcfd1fa14c9ff967245a64d4b063294b7
Adrian Bonislawski <adrian_bon@wp.pl>
2024-02-01 12:57:37 +0100

    add min, max requirements to validation schemas

--- ui/src/api/formSchemas.tsx
+++ ui/src/api/formSchemas.tsx
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 export const projectSchema = yup.object().shape({
-    name: yup.string().required('This field is required'),
-    slug: yup.string().required('This field is required'),
+    name: yup.string().min(3, 'Too short').max(50, 'Too long').required('This field is required'),
+    slug: yup.string().min(3, 'Too short').max(50, 'Too long').required('This field is required'),
     description: yup.string().max(280, 'Too long').required('This field is required'),
     tags: yup.string().required('This field is required'),
     ai_providers: yup.array().min(1, 'You need to add at least one AI Provider')
@@ -10,6 +10,10 @@ export const projectSchema = yup.object().shape({
 export const providerSchema = yup.object().shape({
     api_base: yup.string().url('Enter a valid url').required('This field is required'),
     provider_name: yup.string().required('This field is required'),
-    deployment_name: yup.string().required('This field is required'),
+    deployment_name: yup
+        .string()
+        .min(3, 'Too short')
+        .max(50, 'Too long')
+        .required('This field is required'),
     description: yup.string()
 });
