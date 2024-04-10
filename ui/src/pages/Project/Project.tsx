cff1f4667d37e323fa12abf07aa43f42d686678d
Adrian Bonislawski <adrian_bon@wp.pl>
2024-04-10 14:11:59 +0200

    project details - add view all transactions button

--- ui/src/pages/Project/Project.tsx
+++ ui/src/pages/Project/Project.tsx
@@ -92,7 +92,24 @@ const Project: React.FC & { Add: React.FC; Update: React.FC } = () => {
                 </Flex>
 
                 <Statistics projectId={data.id} />
-                <Container header="Latest transactions" classname={{ parent: 'my-5' }}>
+                <Container
+                    header={
+                        <Flex justify="space-between">
+                            <Title level={2} style={{ margin: '0 10px' }}>
+                                Latest transactions
+                            </Title>
+                            <Button
+                                type="primary"
+                                className="!bg-white my-auto"
+                                onClick={() => navigate(`/transactions?project_id=${data.id}`)}
+                                ghost
+                            >
+                                View all transactions
+                            </Button>
+                        </Flex>
+                    }
+                    classname={{ parent: 'my-5' }}
+                >
                     <LatestTransactions projectId={data.id} />
                 </Container>
             </>
