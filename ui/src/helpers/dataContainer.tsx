f55a1a81b47869d5028a8cf9f6eae5b0366ce3e9
Adrian Bonislawski <adrian_bon@wp.pl>
2024-04-24 13:56:00 +0200

    new ui - tags visibility adjust to container width

--- ui/src/helpers/dataContainer.tsx
+++ ui/src/helpers/dataContainer.tsx
@@ -1,27 +1,79 @@
 import { Popover, Space, Tag } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
     tags: string[];
     classname?: string;
 }
 
-export const TagsContainer: React.FC<Props> = ({ tags, classname }) => (
-    <Space size={0} className={`min-w-40 ${classname}`}>
-        {tags
-            .filter((_el, id) => id < 3)
-            .map((e, id) => (
-                <Tag key={id}>{e}</Tag>
-            ))}
-        {tags.length > 3 && (
-            <Popover
-                content={tags.map((e, id) => (
-                    <Tag key={id}>{e}</Tag>
-                ))}
-                title="Tags"
-                trigger="hover"
-            >
-                <Tag style={{ cursor: 'pointer' }}>...</Tag>
-            </Popover>
-        )}
-    </Space>
-);
+export const TagsContainer: React.FC<Props> = ({ tags, classname }) => {
+    const containerRef = useRef(null);
+    const gap = 8;
+    const tagsJSX: JSX.Element[] = tags.map((el, id) => (
+        <Tag className="m-0" key={id}>
+            {el}
+        </Tag>
+    ));
+    const [visibleTags, setVisibleTags] = useState([] as JSX.Element[]);
+    const [isWidthLoading, setWidthLading] = useState(true);
+    useEffect(() => {
+        const handleTagsWidth = () => {
+            let containerW = 0;
+            const visibleChildren: JSX.Element[] = [];
+            if (containerRef.current) {
+                const container = containerRef.current as HTMLDivElement;
+                const hiddenTags = container.querySelectorAll('.not-visible-tags')[0];
+                if (hiddenTags) {
+                    containerW = container.getBoundingClientRect().width;
+                    hiddenTags.childNodes.forEach((el) => {
+                        const width = (el as HTMLDivElement).getBoundingClientRect().width;
+                        containerW -= width;
+                        if (containerW - (gap * visibleChildren.length + 40) > 0)
+                            visibleChildren.push(
+                                <Tag className="m-0" key={visibleChildren.length}>
+                                    {el.textContent}
+                                </Tag>
+                            );
+                        else return;
+                    });
+                    setVisibleTags(visibleChildren);
+                    setWidthLading(false);
+                }
+            }
+        };
+        handleTagsWidth();
+        setWidthLading(false);
+        window.addEventListener('resize', () => {
+            setWidthLading(true);
+            handleTagsWidth();
+        });
+        return () =>
+            window.removeEventListener('resize', () => {
+                setWidthLading(true);
+                handleTagsWidth();
+            });
+    }, []);
+    return (
+        <Space ref={containerRef} size={gap} className={`min-w-40 ${classname}`}>
+            {visibleTags.map((el) => el)}
+            {visibleTags.length < tags.length && (
+                <Popover
+                    content={
+                        <Space size={gap} className="flex-wrap">
+                            {tagsJSX}
+                        </Space>
+                    }
+                    title="Tags"
+                    trigger="hover"
+                >
+                    <Tag className="cursor-pointer m-0">+{tags.length - visibleTags.length}</Tag>
+                </Popover>
+            )}
+            {isWidthLoading && (
+                <div className="not-visible-tags absolute opacity-0 -top-5">
+                    {tagsJSX.map((el) => el)}
+                </div>
+            )}
+        </Space>
+    );
+};
