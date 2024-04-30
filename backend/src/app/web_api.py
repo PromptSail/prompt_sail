db7f60332698dc6d5719a5cb08f25a1a4e03cf59
Szymon Śliwa <szymon242820@gmail.com>
2024-04-30 19:35:36 +0200

    fix for counting costs from None values when fetching projects

--- backend/src/app/web_api.py
+++ backend/src/app/web_api.py
@@ -51,19 +51,16 @@ from .app import app
 
 @app.get("/api/projects")
 async def get_projects(
-    request: Request,
     ctx: Annotated[TransactionContext, Depends(get_transaction_context)],
 ) -> list[GetProjectSchema]:
     """
     API endpoint to retrieve information about all projects.
 
-    :param request: The incoming request.
     :param ctx: The transaction context dependency.
     :return: A list of GetProjectSchema objects.
     """
     projects = ctx.call(get_all_projects)
-    pricelist = get_provider_pricelist(request)
-
+    
     dtos = []
     for project in projects:
         transactions = ctx.call(get_transactions_for_project, project_id=project.id)
@@ -71,42 +68,8 @@ async def get_projects(
         transaction_count = ctx.call(count_transactions, project_id=project.id)
         if transaction_count > 0:
             for transaction in transactions:
-                price = [
-                    price
-                    for price in pricelist
-                    if re.match(price.match_pattern, transaction.model)
-                ]
-                if (
-                    transaction.input_tokens is not None
-                    and transaction.output_tokens is not None
-                    and transaction.status_code == 200
-                ):
-                    if len(price) > 0:
-                        price = price[0]
-                        if price.total_price > 0:
-                            cost += (
-                                (
-                                    (transaction.input_tokens / 1000)
-                                    + (transaction.output_tokens / 1000)
-                                )
-                                * price.total_price
-                                if transaction.input_tokens > 0
-                                and transaction.output_tokens > 0
-                                else 0
-                            )
-                        else:
-                            cost += (
-                                (transaction.input_tokens / 1000) * price.input_price
-                                if transaction.input_tokens > 0
-                                and price.input_price > 0
-                                else 0
-                            )
-                            cost += (
-                                (transaction.output_tokens / 1000) * price.output_price
-                                if transaction.output_tokens > 0
-                                and price.output_price > 0
-                                else 0
-                            )
+                if transaction.status_code == 200:
+                    cost += transaction.total_cost if transaction.total_cost else 0
         dtos.append(
             GetProjectSchema(
                 **project.model_dump(),
