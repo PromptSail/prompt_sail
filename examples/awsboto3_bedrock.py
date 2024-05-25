f06aeb3e0e6c6b2d986051aa85d04de4eafad503
Szymon Śliwa <58854456+sliwaszymon@users.noreply.github.com>
2024-05-25 04:54:39 +0200

    google vertex request example via simple request + vertexai support

--- examples/awsboto3_bedrock.py
+++ examples/awsboto3_bedrock.py
@@ -2,8 +2,9 @@
 
 # https://aws.amazon.com/blogs/aws/amazon-bedrock-is-now-generally-available-build-and-scale-generative-ai-applications-with-foundation-models/?trk=55dc591c-143b-4ecb-898e-fa3dcd67469e&sc_channel=el
 
-import boto3
 import json
+
+import boto3
 
 # bedrock = boto3.client(service_name="bedrock", region_name="us-east-1")
 
@@ -22,7 +23,7 @@ body = json.dumps(
         "inputText": "Knock, knock!",
     }
 )
-#https://bedrock-runtime.us-east-1.amazonaws.com/+/model/{modelId}/invoke
+# https://bedrock-runtime.us-east-1.amazonaws.com/+/model/{modelId}/invoke
 response = bedrock_runtime.invoke_model(
     body=body, modelId=modelId, accept=accept, contentType=contentType
 )
