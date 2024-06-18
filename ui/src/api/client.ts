4b69a5aa7f8895c375b366a27f79fe8f609f8bab
Adrian Bonislawski <adrian_bon@wp.pl>
2024-06-18 10:36:29 +0200

    fix interceptors

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { ReactNode, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { ReactNode } from 'react';
 import { useError } from '../context/ErrorContext';
 import { useLogin } from '../context/LoginContext';
 
@@ -13,44 +12,41 @@ const client = axios.create({
 });
 
 const AxiosInterceptor: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const navigate = useNavigate();
     const { setLoginState } = useLogin();
     const { hasServerError, setHasServerError } = useError();
-    useEffect(() => {
-        client.interceptors.request.use(
-            (config) => {
-                const token = localStorage.getItem('PS_TOKEN');
-                if (token) {
-                    config.headers['Authorization'] = `Bearer ${token}`;
-                }
-                return config;
-            },
-            (error) => {
-                console.error('inter request error');
-                return Promise.reject(error);
+    client.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem('PS_TOKEN');
+            if (token) {
+                config.headers['Authorization'] = `Bearer ${token}`;
             }
-        );
+            return config;
+        },
+        (error) => {
+            console.error('inter request error');
+            return Promise.reject(error);
+        }
+    );
 
-        client.interceptors.response.use(
-            (response) => {
-                if (!hasServerError) setHasServerError(false);
-                return response;
-            },
-            (error) => {
-                console.error('inter response error');
-                const status = error.response.status;
-                if (status >= 400 && status < 500) {
-                    localStorage.removeItem('PS_TOKEN');
-                    setLoginState(false);
-                }
-                if (status >= 500) {
-                    console.error(error);
-                    setHasServerError(true);
-                }
-                return Promise.reject(error);
+    client.interceptors.response.use(
+        (response) => {
+            if (!hasServerError) setHasServerError(false);
+            return response;
+        },
+        (error) => {
+            console.error('inter response error');
+            const status = error.response.status;
+            if (status >= 400 && status < 500) {
+                localStorage.removeItem('PS_TOKEN');
+                setLoginState(false);
             }
-        );
-    }, [navigate]);
+            if (status >= 500) {
+                console.error(error);
+                setHasServerError(true);
+            }
+            return Promise.reject(error);
+        }
+    );
     return children;
 };
 
